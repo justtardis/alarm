54bf73e394ddbf88553e18771a8d036eb0c59142
Just <justtardis@gmail.com>
2020-05-11 10:03:21 +1000

    Pilot

---
+++ Assets/Scripts/AlarmTrigger.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AlarmTrigger : MonoBehaviour
+{
+    [SerializeField] private DisplayUI _displayUI;
+    [SerializeField] private bool _isPenetrate = false;
+    [SerializeField] private AudioClip _alarmSound;
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private float _maxTime = 3f;
+    [SerializeField] private bool isWorkAlarm = false;
+    [SerializeField] private float _currentTime = 0f;
+
+
+    private void Start()
+    {
+        _audioSource.clip = _alarmSound;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Thief thief))
+        {
+            _isPenetrate = true;
+            _displayUI.SetNewText(_isPenetrate);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Thief thief))
+        {
+            _isPenetrate = false;
+            _displayUI.SetNewText(_isPenetrate);
+        }
+    }
+
+    private void AlarmPlaySound(bool isAlarm, float partTime)
+    {
+        _audioSource.volume += !isAlarm ? partTime : -partTime;
+    }
+
+    public void AP()
+    {
+        _audioSource.PlayOneShot(_alarmSound);
+    }
+
+    private void Update()
+    {
+        float duration = 0f;
+        if (_isPenetrate)
+        {
+            if (!isWorkAlarm)
+            {
+                if (_currentTime >= _maxTime)
+                {
+                    _currentTime = _maxTime;
+                    isWorkAlarm = true;
+                }
+                else
+                {
+                    _currentTime += Time.deltaTime;
+                    duration = _currentTime / _maxTime;
+                    AlarmPlaySound(isWorkAlarm, duration);
+                }
+            }
+            _audioSource.Play();
+        }
+        else
+        {
+            if (isWorkAlarm)
+            {
+                if (_currentTime > 0f)
+                {
+                    _currentTime -= Time.deltaTime;
+                    duration = _currentTime / _maxTime;
+                    AlarmPlaySound(isWorkAlarm, duration);
+                }
+                else
+                {
+                    _currentTime = 0f;
+                    isWorkAlarm = false;
+                }
+                _audioSource.Play();
+            }
+            else
+            {
+                _audioSource.Stop();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/DisplayUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayUI : MonoBehaviour
+{
+    [SerializeField] private Text _textInfo;
+    [SerializeField] private string[] _textSpeeches = { "Жулик снаружи", "Жулик внутри" };
+    private string _textOut;
+
+    private void Start()
+    {
+        _textOut = _textSpeeches[0];
+    }
+
+    public void SetNewText(bool isPenetrate)
+    {
+        _textOut = isPenetrate ? _textSpeeches[1] : _textSpeeches[0];
+    }
+
+    private void Update()
+    {
+        _textInfo.text = _textOut;
+    }
+}
